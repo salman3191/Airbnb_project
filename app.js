f274ee6b6378f6bf4dd608a88794bd219d485757
salman <mohammadsalmankhan3191@gmail.com>
2026-01-13 12:18:33 +0530

    configuring strategy (passport strategies)

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const User = require("./models/user.js");
+const LocalStrategy = require("passport-local");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -44,6 +47,12 @@ sessionOptions = {
 };
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passportsession());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");

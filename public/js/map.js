1b80dc466831a73772600bb8f58f7e74f63afc33
salman <mohammadsalmankhan3191@gmail.com>
2026-01-22 12:43:21 +0530

    set map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,13 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: [74.7973, 34.0837], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9, // starting zoom
 });
+
+console.log(coordinates);
+
+// Create a default Marker and add it to the map.
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .addTo(map);

d7eaf86e8745a6a44d6ec916274a9de44011563d
salman <mohammadsalmankhan3191@gmail.com>
2026-01-22 14:43:51 +0530

    map popup

--- public/js/map.js
+++ public/js/map.js
@@ -10,4 +10,12 @@ console.log(coordinates);
 // Create a default Marker and add it to the map.
 const marker = new mapboxgl.Marker({ color: "red" })
   .setLngLat(coordinates)
+  .setPopup(
+    new mapboxgl.Popup({
+      offset: 25,
+    }).setHTML(`
+      <p>Exact location provided after booking</p>
+      `),
+  )
+
   .addTo(map);
